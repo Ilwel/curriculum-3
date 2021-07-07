b6492573d619e4739bd91ef6e8aad1046ac0081c
ilwel <ilwelisaac@hotmail.com>
2021-07-07 14:33:18 -0300

    hard coded social links

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,13 +8,13 @@ export default function Header() {
                 
                 <span className="c-menu__item">Cartão de Visita</span>
                 <span className="c-menu__item">Projetos</span>
-                <span className="c-menu__item">Experiencia</span>
+                <span className="c-menu__item">Experiência</span>
             
             </aside>
             <aside className="c-social">
-                <a href="" className="c-social__item">Github</a>
-                <a href="" className="c-social__item">Linkedin</a>
-                <a href="" className="c-social__item">Instagram</a>
+                <a href="https://github.com/Ilwel" target="_blank" className="c-social__item">Github</a>
+                <a href="https://www.linkedin.com/in/ilwel/" target="_blank" className="c-social__item">Linkedin</a>
+                <a href="https://www.instagram.com/_ilwel/" target="_blank" className="c-social__item">Instagram</a>
             </aside>
 
         </header>
