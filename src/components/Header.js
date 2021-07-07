f1d0c2826eb52c86552c20842a98adc31fce115c
ilwel <ilwelisaac@hotmail.com>
2021-07-07 12:21:59 -0300

    header react component

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+
+export default function Header() {
+
+    return (
+
+        <header className="l-header">
+            <aside className="c-menu">
+                
+                <span className="c-menu__item">Cartão de Visita</span>
+                <span className="c-menu__item">Projetos</span>
+                <span className="c-menu__item">Experiencia</span>
+            
+            </aside>
+            <aside className="c-social">
+                <a href="" className="c-social__item">Github</a>
+                <a href="" className="c-social__item">Linkedin</a>
+                <a href="" className="c-social__item">Instagram</a>
+            </aside>
+
+        </header>
+
+    )
+
+}
