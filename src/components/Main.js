51d5a6b069b48fd497aca241a7527b329d34659c
ilwel <ilwelisaac@hotmail.com>
2021-07-07 12:22:16 -0300

    main react component

---
+++ src/components/Main.js
@@ -0,0 +1,17 @@
+
+export default function Main(){
+
+    return(
+
+        <main className="l-main">
+            <aside className="c-hero">
+
+            </aside>
+            <aside className="c-content">
+                
+            </aside>
+        </main>
+
+    )
+
+}
