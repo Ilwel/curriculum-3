3599a2e389ab147630af27e65f2497c2b260fe95
ilwel <ilwelisaac@hotmail.com>
2021-07-10 10:04:10 -0300

    Business Card logic/content

--- src/components/BusinessCard.js
+++ src/components/BusinessCard.js
@@ -1,15 +1,141 @@
+import tecnologies from "../utils/tecnologies";
+import perfil from '../assets/perfil.jpg';
 
-export default function BusinessCard(){
+export default function BusinessCard({ side, tecnologie, mouseclickCardTecItem }) {
 
-    return(
+    const { infos } = tecnologie;
+
+    const mouseOverCardTecItem = (e) => {
+
+        e.target.classList.add("is-hover");
+        const items = document.getElementsByClassName("c-card__tec-item");
+        for (const item of items) {
+
+            item.classList.add("is-blur");
+
+        }
+        e.target.classList.remove("is-blur");
+
+    }
+    const mouseLeaveCardTecItem = (e) => {
+
+        e.target.classList.remove("is-hover");
+        const items = document.getElementsByClassName("c-card__tec-item");
+        for (const item of items) {
+
+            item.classList.remove("is-blur");
+
+        }
+
+    }
+
+    const mouseOverProgressBar = (e) => {
+
+        // e.target.classList.add("is-hover");
+        const items = document.getElementsByClassName("c-card__knowledg-item");
+        for (const item of items) {
+
+            item.classList.add("is-blur");
+
+        }
+        e.target.classList.remove("is-blur");
+
+    }
+    const mouseLeaveProgressBar = (e) => {
+
+        // e.target.classList.remove("is-hover");
+        const items = document.getElementsByClassName("c-card__knowledg-item");
+        for (const item of items) {
+
+            item.classList.remove("is-blur");
+
+        }
+
+    }
+    return (
 
         <div className="c-card">
-        
-            <h1 className="c-card__title">Ilwel Isaac</h1>
-            <h2 className="c-card__sub">Web developer</h2>
-            <p>ilwelisaac@hotmail.com</p>
-            <p>(85) 9 8105-0749</p>
-            <p>Fortaleza, CE</p>
+
+            {side === "left" && (
+                <>
+                    <div className="c-card__about">
+                        <img className="c-card__img" src={perfil} alt="imagem de perfil" />
+                        <div className="c-card__infos left">
+
+                            <h1 className="c-card__title">Ilwel Isaac</h1>
+                            <h2 className="c-card__sub">Web developer</h2>
+                            <p className="c-card__p">ilwelisaac@hotmail.com</p>
+                            <p className="c-card__p">(85) 9 8105-0749</p>
+                            <p className="c-card__p">Fortaleza, CE</p>
+
+                        </div>
+
+                    </div>
+
+
+                    <div className="c-card__tec">
+                        {tecnologies.map(item =>
+                            <span
+                                id={item.tag}
+                                onMouseOver={e => mouseOverCardTecItem(e)}
+                                onMouseLeave={e => mouseLeaveCardTecItem(e)}
+                                onClick={e => mouseclickCardTecItem(e)}
+                                className={`c-card__tec-item ${item.tag}`}>
+
+                                {item.name}
+
+                            </span>
+                        )}
+                    </div>
+
+                    <p className="c-card__signature">Business Card</p>
+
+                </>
+            )}
+
+            {side === "right" && (
+                <>
+
+                    <div className="c-card">
+
+                        <h1 className="c-card__title">
+                            {tecnologie.name}
+                        </h1>
+                        <h2 className="c-card__sub">
+                            {infos.about}
+                        </h2>
+
+                        <ul className="c-card__knowledg">
+
+                            {infos.knowledg.map(item => (
+                                <li
+                                    key={item.name + tecnologie.name}
+                                    className="c-card__knowledg-item"
+                                    onMouseOver={mouseOverProgressBar}
+                                    onMouseLeave={mouseLeaveProgressBar}
+                                >
+
+                                    {item.name}
+                                    <div
+                                        className="c-card__progress-bar"
+                                        style={
+                                            {
+                                                width: item.expertise
+                                            }
+                                        }
+                                    >
+
+                                    </div>
+                                </li>))}
+
+                        </ul>
+
+                    </div>
+
+                </>
+            )}
+
+
 
         </div>
 
