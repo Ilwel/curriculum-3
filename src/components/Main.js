e67edd81f214d8df37856e5785ae862941f56618
ilwel <ilwelisaac@hotmail.com>
2021-07-10 10:02:42 -0300

    useState Hook -- tecnologie state

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,24 +1,50 @@
-import BusinessCard from "./BusinessCard"
+import BusinessCard from "./BusinessCard";
+import tecnologies from "../utils/tecnologies";
+import { useState } from "react";
 
-export default function Main({ menu }){
+export default function Main({ menu }) {
+    const [tecnologie, setTecnologie] = useState({infos:{knowledg:[]}});
 
-    return(
+    const mouseclickCardTecItem = (e) => {
+
+        const { target: { id } } = e;
+        const newTecnologie = tecnologies.find(item => item.tag === id);
+        setTecnologie(newTecnologie);
+        console.log(tecnologie.name)
+
+    }
+
+    return (
 
         <main className="l-main">
             <aside className="c-hero">
 
-                {menu[0] && <BusinessCard/>}
+                {menu[0] && (
+
+                    <BusinessCard
+                        side="left"
+                        tecnologie={tecnologie}
+                        mouseclickCardTecItem={mouseclickCardTecItem}
+                    />
+
+                )}
                 {menu[1] && 'Projetos'}
                 {menu[2] && 'Experiência'}
 
             </aside>
 
             <aside className="c-content">
-            
-                {menu[0] && 'Cartão de visita'}
+
+                {menu[0] && (
+                    <BusinessCard
+                        side="right"
+                        tecnologie={tecnologie}
+                        mouseclickCardTecItem={mouseclickCardTecItem}
+                    />
+                )}
                 {menu[1] && 'Projetos'}
                 {menu[2] && 'Experiência'}
-            
+
             </aside>
         </main>
 
