4ff8c7dfc95ead009d369d4ded08c9837fb3cba1
ilwel <ilwelisaac@hotmail.com>
2021-07-10 10:05:59 -0300

    Business Card utils

---
+++ src/utils/tecnologies.js
@@ -0,0 +1,87 @@
+
+export default [
+    {
+        name: "Javascript",
+        tag: "js",
+        infos: {
+            about:"Javascript é a linguagem que me possibilitou aprender de forma rápida como desenvolver para web. Foquei em estender as especificidades da linguagem para tirar todo potencial dela",
+            knowledg:[
+                { name:"Sintaxe geral da linguagem", expertise: "80%" },
+                { name:"Callbacks", expertise: "75%" },
+                { name:"Tipagem e declaração de variáveis", expertise: "100%" },
+                { name:"Function statemets", expertise: "80%" },
+                { name:"Classes and Objects", expertise: "70%" },
+                { name:"Promisses", expertise: "75%" },
+            ]
+        }
+    },
+    { 
+        name: "CSS", 
+        tag: "css",
+        infos: {
+            about:"Estudei a fundo CSS e boas práticas de escrita de folhas de estilo para ter projetos bem desenvolvidos",
+            knowledg:[
+                { name:"Técnicas CSS em geral", expertise: "90%" },
+                { name:"Flex", expertise: "90%" },
+                { name:"Grid", expertise: "70%" },
+                { name:"Box-Model", expertise: "100%" },
+                { name:"BEM", expertise: "70%" },
+                { name:"SMACSS", expertise: "60%" },
+            ]
+        }
+    },
+    { 
+        name: "HTML", 
+        tag: "html",
+        infos: {
+            about:"HTML é essencial para o desenvolvimento de páginas web, me  prestei a  entender como a hierarquia funciona dentro dessa linguagem",
+            knowledg:[
+                { name:"Sintaxe geral da linguagem", expertise: "80%" },
+                { name:"Hierarquia de construção", expertise: "95%" },
+                { name:"Tags semânticas", expertise: "85%" },
+                { name:"Boas práticas de escrita", expertise: "80%" },
+            ]
+        }
+    },
+    { 
+        name: "C++", 
+        tag: "cplus",
+        infos: {
+            about:"C foi a linguagem com a qual eu aprendi a programar. C++ me ajudou a complementar várioss conhecimentos fundamentais em lógica de programação",
+            knowledg:[
+                { name:"Sintaxe geral da linguagem", expertise: "80%" },
+                { name:"Manipulação e alocação de memória", expertise: "70%" },
+                { name:"Programação estruturada", expertise: "100%" },
+                { name:"Programação Orientada a Objeto", expertise: "80%" },
+                { name:"Estrutura de dados", expertise: "70%" },
+            ]
+        }   
+    },
+    { 
+        name: "Express",
+        tag: "express",
+        infos: {
+            about:"Express é um Framework utilizado para criar aplicações REST com Javascript e NodeJs. Foi nele que eu tive contato com todas as dinâmicas de uma aplicação REST no backend",
+            knowledg:[
+                { name:"Conhecimento e aplicação do Framework", expertise: "70%" },
+                { name:"Padrão REST", expertise: "80%" },
+                { name:"Controladores e Roteadores", expertise: "90%" },
+            ]
+        }
+    },
+    { 
+        name: "React", 
+        tag: "react",
+        infos: {
+            about:"React é um dos mais populares Frameworks para aplicações frontend, com ele eu tenho estudado a ciração de páginas que seguem o modelo SPA (Single Page Aplication) e todos os pormenores de desenvolvimento e integração de aplicações frontend",
+            knowledg:[
+                { name:"Conhecimento e aplicação do Framework", expertise: "60%" },
+                { name:"Controle de Estados", expertise: "70%" },
+                { name:"Hooks", expertise: "60%" },
+                { name:"Componentização", expertise: "80%" },
+                { name:"Contexto", expertise: "60%" },
+            ]
+        }
+    },
+
+]
